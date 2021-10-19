a715d2cfb76d03db50f424d10c8af55b2b7eb494
Poojan Patel <poojan.patel1126@gmail.com>
2021-10-19 15:23:28 -0700

    Added special buttons

--- ViewController.swift
+++ ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     var answer:String = ""
     var text:String = ""
     
+    
     // Answer Label outlet
     @IBOutlet weak var AnswerLabel: UILabel!
     
@@ -33,6 +34,7 @@ class ViewController: UIViewController {
         
         // If condition to check if operator is present
         if(curr_operator == "" && answer == ""){
+            print("IFFFFFFFFFF")
         
             switch answer_text {
             case ".":
@@ -50,50 +52,49 @@ class ViewController: UIViewController {
                 
             }
             AnswerLabel.text = text
+            operand1.append(text)
         }
         else{
-            if (answer == ""){
-                operand1.append(text)
-                
-                
-            }
-            else{
+            if (answer != ""){
                 operand1 = ""
                 operand1.append(answer)
-                operand2 = ""
+
+
             }
             
-            text = ""
+            print("ELSEEEEEE")
             AnswerLabel.text = "0"
             switch answer_text {
             case ".":
-                if !text.contains(".")
+                if !operand2.contains(".")
                 {
-                    text.append(answer_text)
+                    operand2.append(answer_text)
                 }
             case "0":
-                if text != "0"
+                if operand2 != "0"
                 {
-                    text.append(answer_text)
+                    operand2.append(answer_text)
                 }
             default:
-                text.append(answer_text)
+                operand2.append(answer_text)
                 
             }
-            AnswerLabel.text = text
-            operand2.append(text)
+            AnswerLabel.text = operand2
+            print(operand2, operand1)
+            //operand2.append(text)
         }
     }
     
-// function for opererator buttons
+// function for operator buttons
     @IBAction func OnOperatorClick(_ sender: UIButton) {
         
         var btn_text = sender.titleLabel!.text!
+        print(curr_operator)
         
         // Removing previous operators used
         if (curr_operator != ""){
         
-            //print(curr_operator,operand1,operand2)
+            print(curr_operator,operand1,operand2)
             if (operand1 != "" && operand2 != "") {
                 if (curr_operator == "+"){
                     var output = add(a:operand1,b:operand2)
@@ -114,7 +115,12 @@ class ViewController: UIViewController {
                 
                 answer = ""
                 answer.append(AnswerLabel.text!)
+                operand2 = ""
+                operand1 = ""
+                operand1.append(answer)
                 curr_operator = ""
+                curr_operator.append(btn_text)
+                
             }
         }
         else {
@@ -124,39 +130,129 @@ class ViewController: UIViewController {
     }
     
     
-    // Clear Button
+    // Percentage button
     
-    @IBAction func ClearButton(_ sender: UIButton) {
+    @IBAction func OnPercentageClick(_ sender: UIButton) {
+        var output = percentage(a: operand1)
+        AnswerLabel.text = output
+        answer = ""
+        answer = AnswerLabel.text!
+        print(output)
+    }
+    
+    // +/- Button functionality
+    @IBAction func OnPiusMinusClick(_ sender: UIButton) {
+        print(AnswerLabel.text!.prefix(1))
+        var output = negate(a:AnswerLabel.text!)
+        AnswerLabel.text = output
+        if answer != "" {
+            answer = ""
+            answer.append(AnswerLabel.text!)
+        }
         
     }
+    
+    // Backspace Button
+    @IBAction func OnBackSpace(_ sender: UIButton) {
+        
+        var b_text = AnswerLabel.text
+        // Used droplast function to drop last character
+        b_text = String(b_text!.dropLast())
+        AnswerLabel.text = b_text
+        if (answer != ""){
+            answer = ""
+            answer.append(b_text!)
+        }
+        // If the last number from answer is removed then replace with 0
+        if AnswerLabel.text == "" {
+            AnswerLabel.text?.append("0")
+        }    }
+    
+    
+    // Clear Button
+    @IBAction func ClearButton(_ sender: UIButton) {
+        operand1 = ""
+        operand2 = ""
+        curr_operator = ""
+        text = ""
+        answer = ""
+        AnswerLabel.text = "0"
+    }
+    
+    // Equal Button
+    @IBAction func OnEqualClick(_ sender: UIButton) {
+        
+        if (curr_operator != ""){
+        
+            print(curr_operator,operand1,operand2)
+            if (operand1 != "" && operand2 != "") {
+                if (curr_operator == "+"){
+                    var output = add(a:operand1,b:operand2)
+                    AnswerLabel.text = output
+                }
+                else if (curr_operator == "-"){
+                    var output = subtract(a:operand1,b:operand2)
+                    AnswerLabel.text = output
+                }
+                else if (curr_operator == "X"){
+                    var output = multiply(a:operand1,b:operand2)
+                    AnswerLabel.text = output
+                }
+                else if (curr_operator == "/"){
+                    var output = divide(a:operand1,b:operand2)
+                    AnswerLabel.text = output
+                }
+                
+                answer = ""
+                answer.append(AnswerLabel.text!)
+                operand2 = ""
+                operand1 = ""
+                operand1.append(answer)
+                curr_operator = ""
+                
+            }
+        }
+    }
+    
     // Functions to perform Mathematical operations
     func add(a:String, b:String) -> String{
         var result: Float = Float(a)! + Float(b)!
-        var answer1 = String(result)
+        var answer1 = String(format:"%.8f",result)
         return answer1
         
     }
     func subtract(a:String, b:String) -> String{
         var result: Float = Float(a)! - Float(b)!
-        var answer1 = String(result)
+        var answer1 = String(format:"%.8f",result)
         return answer1
         
     }
 
     func multiply(a:String, b:String) -> String{
         var result: Float = Float(a)! * Float(b)!
-        var answer1 = String(result)
+        var answer1 = String(format:"%.8f",result)
         return answer1
         
     }
 
     func divide(a:String, b:String) -> String{
         var result: Float = Float(a)! / Float(b)!
-        var answer1 = String(result)
+        var answer1 = String(format:"%.8f",result)
         return answer1
         
     }
-
+    
+    func negate(a:String) -> String{
+        var result: Float = Float(a)! * -1
+        var answer1 = String(result)
+        return answer1
+    }
+    
+    func percentage(a:String) -> String{
+        var result: Float = Float(a)! / 100
+        var answer = String(result)
+        return answer
+    }
 }
 
     
